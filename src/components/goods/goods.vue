9a04718352e5abae181ea1b671bdb6211bc1979d
Cleam Lee <lizhigao@021.com>
2017-03-19 23:25:10 +0800

    inti

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="goods">
+    <p>goods!!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goods',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style type="stylus" rel="text/stylus">
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <p>header!!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style type="stylus" rel="text/stylus">
+
+</style>
